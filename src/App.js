2debf29524359839d8cc918c396ee70a5a3e0572
Isabelle <isabel.tartarelli@live.it>
2024-01-05 20:07:23 +0000

    Completed Nav Bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ function App() {
 	return (
 		<div className="App">
 			<header>
-       <img className="bkg-img" style={{ backgroundImage: `url(${background})` }} alt=""/>
+				<img
+					className="bkg-img"
+					style={{ backgroundImage: `url(${background})` }}
+					alt=""
+				/>
+				<div className="bkg-shadow">
+					<h1>Welcome</h1>
+					<h2>
+						I'm Isabelle, a Full-Stack Software Developer living in Scotland.
+					</h2>
+					<div className="button-container">
+						<button>About Me</button>
+						<button>Projects</button>
+						<button>Skills</button>
+						<button>Contacts</button>
+					</div>
+				</div>
 			</header>
 		</div>
 	)
