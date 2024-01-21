52289e2d902f44a01ac97b07c5def72b376a6aca
Isabelle <isabel.tartarelli@live.it>
2024-01-21 17:16:00 +0000

    Added grid for About me

--- src/App.js
+++ src/App.js
@@ -25,23 +25,25 @@ function App() {
 				</div>
 			</header>
 			<main>
-        <div className="main-bkg">
-          <div className="main-container">
-            <h1 className="about-me"> A Bit About Me</h1>
-					<img className="profile-pic" src={profilePic} alt="" />
-					<p>
-						{' '}
-						My journey into the world of technology was preceded by a
-						distinguished 10-year career in the hospitality industry, where I
-						honed my skills as a chef. This unique blend of experience has
-						equipped me with a deep understanding of teamwork, time management,
-						adaptability, and attention to detail; qualities that seamlessly
-						translate into my career as a software developer. I am excited to
-						leverage my passion for technology and my newly acquired software
-						development knowledge to excel as a junior software developer.
-					</p>
-          </div>
+				<div className="main-bkg">
+					<div className="about-me-container">
 					
+							<h1 className="about-me-title"> A Bit About Me</h1>
+							<img className="about-me-image" src={profilePic} alt="" />
+							<p className="about-me-text">
+								{' '}
+								My journey into the world of technology was preceded by a
+								distinguished 10-year career in the hospitality industry, where
+								I honed my skills as a chef. This unique blend of experience has
+								equipped me with a deep understanding of teamwork, time
+								management, adaptability, and attention to detail; qualities
+								that seamlessly translate into my career as a software
+								developer. I am excited to leverage my passion for technology
+								and my newly acquired software development knowledge to excel as
+								a junior software developer.
+							</p>
+					
+					</div>
 				</div>
 			</main>
 		</div>
