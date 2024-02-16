ade836f365905a89d2212913d70c81baa7b2bd7d
Isabelle <isabel.tartarelli@live.it>
2024-02-16 14:25:24 +0000

    Installed 'react-router-dom' package and added HyperLink to the Resume Button

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,11 +1,11 @@
 import './AboutMe.css'
 import profilePic from '../../pictures/foto.png'
-
+import { Link } from 'react-router-dom'
 
 const AboutMe = () => {
 	return (
 		<main>
-			<div className="about-me-container" id='aboutMe' >
+			<div className="about-me-container" id="aboutMe">
 				<h1 className="about-me-title"> A Bit About Me</h1>
 				<img className="about-me-image" src={profilePic} alt="" />
 				<p className="about-me-text">
@@ -19,8 +19,9 @@ const AboutMe = () => {
 					leverage my passion for technology and my newly acquired software
 					development knowledge to excel as a junior software developer.
 				</p>
-
-				<button className="resume-button">Resume</button>
+				<Link to="/Resume" className="resume-button">
+					<button className="resume-button">Resume</button>
+				</Link>
 			</div>
 		</main>
 	)
--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,25 +1,8 @@
 import './ScrollToTop.css'
 import arrowup from '../../pictures/Arrow Up.png'
-import React, { useState, useEffect } from 'react'
 
 const ScrollToTop = () => {
-	// const [showTopBtn, setShowTopBtn] = useState(false)
-	
-	// useEffect(() => {
-	// 	window.addEventListener('scroll', () => {
-	// 		if (window.scrollY > 400) {
-	// 			setShowTopBtn(true)
-	// 		} else {
-	// 			setShowTopBtn(false)
-	// 		}
-	// 	})
-	// }, [])
-	// const goToTop = () => {
-	// 	window.scrollTo({
-	// 		top: 0,
-	// 		behavior: 'smooth',
-	// 	})
-	// }
+
 	const scroll = () => {
 		//0, 0 just takes us to the top of the page
 		window.scrollTo(0, 0)
