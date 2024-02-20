94afcafc28e027b40cc2c99ddac7f68e87c79608
Isabelle <isabel.tartarelli@live.it>
2024-02-20 17:43:34 +0000

    Added line in AboutMe section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,14 +12,15 @@ const AboutMe = () => {
 				<img className="about-me-image" src={profilePic} alt="" />
 				<p className="about-me-text">
 					{' '}
-					My journey into the world of technology was preceded by a
-					distinguished 10-year career in the hospitality industry, where I
-					honed my skills as a chef. This unique blend of experience has
-					equipped me with a deep understanding of teamwork, time management,
-					adaptability, and attention to detail; qualities that seamlessly
-					translate into my career as a software developer. I am excited to
-					leverage my passion for technology and my newly acquired software
-					development knowledge to excel as a junior software developer.
+					I'm Isabelle, a Full-Stack Software Developer living in Scotland.
+					My journey into the world of technology was preceded by a distinguished
+					10-year career in the hospitality industry, where I honed my skills as
+					a chef. This unique blend of experience has equipped me with a deep
+					understanding of teamwork, time management, adaptability, and
+					attention to detail; qualities that seamlessly translate into my
+					career as a software developer. I am excited to leverage my passion
+					for technology and my newly acquired software development knowledge to
+					excel as a junior software developer.
 				</p>
 				<button className="resume-button" onClick={() => navigate('/resume')}>
 					Resume
