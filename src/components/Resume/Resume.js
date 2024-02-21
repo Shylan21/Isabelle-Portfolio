a4b15643e56acd8feb29f9c9bb08b63f4a1a212e
Isabelle <isabel.tartarelli@live.it>
2024-02-21 17:29:55 +0000

    Created OneDevAway Component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -9,15 +9,18 @@ import BooleanEd from './Education/Boolean'
 import WebDev from './Education/WebDevelopment'
 import IntroToCode from './Education/IntroToCoding'
 import Diploma from './Education/Diploma'
+import OneDev from './WorkExperience/OneDevAway'
 
 const Resume = () => {
 	return (
-		<div>
+		<div className="container">
 			{/* TOP BAR  */}
 			<TopBar />
 			{/* SECTION - WORK */}
 			<section className="exp-container">
 				<h1 className="work-exp-title">Work Experience</h1>
+				{/* ONEDEVAVAY */}
+				<OneDev />
 				{/* BOOLEAN */}
 				<BooleanExp />
 				{/* CHEF */}
@@ -28,8 +31,8 @@ const Resume = () => {
 				<h1 className="education-title"> Education</h1>
 				<BooleanEd />
 				<WebDev />
-				<IntroToCode />
-        <Diploma/> 
+				{/* <IntroToCode />
+        <Diploma/>  */}
 			</section>
 			{/* FOOTER */}
 			<Contacts />
---
+++ src/components/Resume/WorkExperience/OneDevAway.js
@@ -0,0 +1,23 @@
+import '../Resume.css'
+
+const OneDev = () => {
+	return (
+		<div id="onedev-exp">
+			<span className="date"> 09/2023 - Current </span>
+			<h1 className="titles"> Freelance Full-Stack Developer </h1>
+			<h2 className="titles"> OneDevAway </h2>
+			<h4 className="titles"> Edinburgh, Scotland </h4>
+			<ul>
+				<li>
+					Developed a full-stack e-commerce website. Managed carousels features,
+					overlooked translations.
+				</li>
+				<li>
+					Currently working on Figma design for a book customization website.
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default OneDev
